a2fe147db23eb79288eaa6ba4e9ced47469b59a5
Sal8298 <salahuddinasim95@gmail.com>
2022-11-08 14:21:33 -0500

    Created inital variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordLength;
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar = ["!", "@", "#", "$","%", "&", "*", ";"];
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +17,21 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+/*
+
+Choose length of password
+
+Choose numbers
+
+choose uppercase
+
+choooses lowercase
+
+choose specialchars
+
+generate password
+
+*/
